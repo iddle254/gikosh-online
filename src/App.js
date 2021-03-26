d3fa1e2a455a8a3eed5332fc2866f394cbe1fb65
Charles <charlesiddle99@gmail.com>
2021-03-26 17:58:12 +0300

    moving to the shop-page component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.pages";
+import Shop from "./pages/shoppage/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={Shop} />
         {/* <Route exact path="" component={} />
       <Route exact path="" component={} /> */}
       </Switch>
