4556da76b93cb63e3c0c5546a0d64d6d5d9f3a5d
Charles <charlesiddle99@gmail.com>
2021-03-26 16:30:25 +0300

    added routing with react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage.pages";
+import { Route, Switch } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.pages";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        {/* <Route exact path="" component={} />
+      <Route exact path="" component={} /> */}
+      </Switch>
     </div>
   );
 }
