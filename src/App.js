1c71ada0cd3e0755f6dcf70e44a1fbd504b5d3c9
Charles <charlesiddle99@gmail.com>
2021-03-29 09:49:12 +0300

    linked to the header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.pages";
 import Shop from "./pages/shoppage/shop.component";
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop} />
